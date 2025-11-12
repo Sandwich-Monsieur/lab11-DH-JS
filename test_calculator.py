99cf872e157cf36b9834d97e71611e50800caeb2
Sandwich-Monsieur <dommiafl@gmail.com>
2025-11-12 08:29:20 -0500

    Finalized it

--- test_calculator.py
+++ test_calculator.py
@@ -20,11 +20,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):  # 3 assertions
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-2, 3), -6)
+        self.assertEqual(mul(0, 3), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self):  # 3 assertions
+        self.assertEqual(div(4, 2), 0.5)
+        self.assertEqual(div(-4, 2), -0.5)
+        self.assertRaises(ZeroDivisionError, div, 0, 0)
     # ##########################
 
     #Partner 2
@@ -45,21 +49,19 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):  # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(0, -5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):  # 3 assertions
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(-3, 4), 5)
+        self.assertEqual(hypotenuse(0, 2), 2)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self):  # 3 assertions
+        self.assertRaises(ValueError, square_root, -1)
+        self.assertEqual(square_root(0), 0)
+        self.assertEqual(square_root(4), 2)
     ##########################
 
 # Do not touch thi
