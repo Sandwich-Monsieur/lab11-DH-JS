a0d80b077e8631dfc18f37f6e79dc224c10b88de
serrano <serranojose0506@gmail.com>
2025-11-12 07:29:51 -0500

    Added functions

--- calculator.py
+++ calculator.py
@@ -2,11 +2,22 @@
 #Partner 1: Dominic Hung
 #Partner 2: Jose Serrano
 
+import math
+def add(a, b): a + b
+
+def sub(a, b): a - b
+
+def mul(a, b): a * b
+
+def div(a, b): b / a # raise ZeroDivisionError if a == 0
+
+def log(a, b): loga(b)# use math library + raise ValueError
+
+def exp(a, b): a^b
 
 """
 calculator.py
 - Defines functions used to create a simple calculator
-
 One function per operation, in order.
 """
 # First example
